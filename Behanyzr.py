39d9e79dbce77c14c021830b9070f4a8571ccbf7
3RR05 <jaiakash922@gmail.com>
2026-02-15 12:05:14 +0530

    Checkpoint-1 : Project Structure and Flask Setup Has Been Done

---
+++ Behanyzr.py
@@ -0,0 +1,12 @@
+from App import create_app, db
+
+app = create_app()
+
+@app.cli.command()
+def init_db():
+    # Initializing DB
+    db.create_all()
+    print("DB Initialized! `~`")
+    
+if __name__ == '__main__':
+    app.run(debug= True, host= '0.0.0.0', port= 5000)
